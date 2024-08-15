caa3f7601d03883cf0541746f939897a60ca7499
akeeb12 <112759560+akeeb12@users.noreply.github.com>
2024-08-15 11:16:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,7 +2,32 @@ const arr = [1, 2, 3, 4, 1, 0, 2, 2];
 
 const divide = (arr, n) => {
   // Write your code here
+
+	const result=[];
+	let currentSubarray [];
+	let currentSum  = 0;
+
+	for(const num of arr)
+	{
+		if(currentSum + sum > n)
+		{
+			result.push(currentSubarray);
+			currentSubarray = [num];
+			currentSum = num;
+		}else
+		{
+			currentSubarray.push(num);
+			currentSum += num;
+		}
+	}
+	if(currentSubarray.length) 
+		result.push(currentSubarray);
+		
 };
 
 const n = prompt("Enter n: ");
 alert(JSON.stringify(divide(arr, n)));
+
+
+
+
